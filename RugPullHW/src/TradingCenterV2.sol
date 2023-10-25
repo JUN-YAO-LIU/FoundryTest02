a35f63c98ba8a04a3d7026bb929af387bb82e755
JimLiu <jim.liu123@ehsn.com.tw>
2023-10-25 10:54:42 +0800

    add initialize()

--- RugPullHW/src/TradingCenterV2.sol
+++ RugPullHW/src/TradingCenterV2.sol
@@ -3,4 +3,10 @@ pragma solidity ^0.8.17;
 
 // TODO: Try to implement TradingCenterV2 here
 contract TradingCenterV2 {
+    bool private initialized;
+    function initialize() public {
+        require(!initialized, "Contract instance has already been initialized");
+        initialized = true;
+        x = _x;
+    }
 }
