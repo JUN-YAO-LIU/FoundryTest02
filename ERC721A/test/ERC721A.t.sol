58cc4d5e5dc013a5c7325ca70243e62070d380e8
劉駿嶢 <liujunyao@liujunyaodeMacBook-Pro.local>
2023-11-05 14:26:53 +0800

    test 10

--- ERC721A/test/ERC721A.t.sol
+++ ERC721A/test/ERC721A.t.sol
@@ -20,19 +20,46 @@ contract ERC721A is Test {
         assertEq(jimTokenA.balanceOf(jim), 1);
     }
 
+    function test_10_mint() public {
+        vm.startPrank(jim);
+        jimTokenA.mint(10);
+        vm.stopPrank();
+        assertEq(jimTokenA.balanceOf(jim), 10);
+    }
 
     function test_transfer() public {
         vm.startPrank(jim);
-        jimTokenA.mint(10);
-        jimTokenA.safeTransferFrom(jim,bob,2);
+        jimTokenA.mint(1);
+        jimTokenA.safeTransferFrom(jim,bob,0);
         vm.stopPrank();
-        assertEq(jimTokenA.ownerOf(2), bob);
+        assertEq(jimTokenA.ownerOf(0), bob);
+    }
+
+
+    function test_10_transfer() public {
+        vm.startPrank(jim);
+        jimTokenA.mint(10);
+        for(uint8 i =0 ;i < 10;i++){
+            jimTokenA.safeTransferFrom(jim,bob,i);
+        }
+        vm.stopPrank();
+        assertEq(jimTokenA.balanceOf(bob), 10);
     }
 
     function test_approve() public {
         vm.startPrank(jim);
         jimTokenA.mint(10);
         jimTokenA.approve(bob,1);
+        vm.stopPrank();
+        assertEq(jimTokenA.getApproved(1), bob);
+    }
+
+    function test_10_approve() public {
+        vm.startPrank(jim);
+        jimTokenA.mint(10);
+        for(uint8 i =0 ;i < 10;i++){
+            jimTokenA.approve(bob,i);
+        }
         vm.stopPrank();
         assertEq(jimTokenA.getApproved(1), bob);
     }
--- ERC721A/test/ERC721Jim.t.sol
+++ ERC721A/test/ERC721Jim.t.sol
@@ -20,6 +20,15 @@ contract ERC721Jim is Test {
         assertEq(jimToken.balanceOf(jim), 1);
     }
 
+    function test_10_mint() public {
+        vm.startPrank(jim);
+        for(uint8 i =0 ;i < 10;i++){
+            jimToken.mint();
+        }
+        vm.stopPrank();
+        assertEq(jimToken.balanceOf(jim), 10);
+    }
+
     function test_transfer() public {
         vm.startPrank(jim);
         jimToken.mint();
@@ -28,10 +37,30 @@ contract ERC721Jim is Test {
         assertEq(jimToken.ownerOf(1), bob);
     }
 
+    function test_10_transfer() public {
+        vm.startPrank(jim);
+         for(uint8 i =0 ;i < 10;i++){
+            jimToken.mint();
+            jimToken.safeTransferFrom(jim,bob,1+i);
+        }
+        vm.stopPrank();
+        assertEq(jimToken.balanceOf(bob), 10);
+    }
+
     function test_approve() public {
         vm.startPrank(jim);
         jimToken.mint();
         jimToken.approve(bob,1);
+        vm.stopPrank();
+        assertEq(jimToken.getApproved(1), bob);
+    }
+
+    function test_10_approve() public {
+        vm.startPrank(jim);
+        for(uint8 i =0 ;i < 10;i++){
+            jimToken.mint();
+            jimToken.approve(bob,i+1);
+        }
         vm.stopPrank();
         assertEq(jimToken.getApproved(1), bob);
     }
